e783ce8b551edb0208e7c8973fa0fba31b900334
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-09-03 01:51:16 -0300

    feat: add `Entity` base class

---
+++ Back-End/Lancheria/Lancheria.Domain/Entities/Entity.cs
@@ -0,0 +1,9 @@
+﻿namespace Lancheria.Domain.Entities
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+    }
+}
