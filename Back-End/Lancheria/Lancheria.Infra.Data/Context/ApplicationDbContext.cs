c031442cf4b0312c5c5132ab0d06ea467781bd61
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-09-22 02:07:38 -0300

    feat: add application DB context

---
+++ Back-End/Lancheria/Lancheria.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Lancheria.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lancheria.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
