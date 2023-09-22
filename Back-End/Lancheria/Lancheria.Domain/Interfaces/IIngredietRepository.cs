caa7ebb9c30c412a8379cb7473cd75b061b37fa4
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-09-22 01:33:04 -0300

    feat: add ingredient respository

---
+++ Back-End/Lancheria/Lancheria.Domain/Interfaces/IIngredietRepository.cs
@@ -0,0 +1,18 @@
+﻿using Lancheria.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lancheria.Domain.Interfaces
+{
+    public interface IIngredietRepository
+    {
+        Task<IEnumerable<Ingredient>> GetIngredientsAsync();
+        Task<Ingredient> GetByIdAsync(int id);
+        Task<Ingredient> CreateAsync(Ingredient ingredient);
+        Task<Ingredient> UpdateAsync(Ingredient ingredient);
+        Task<Ingredient> DeleteAsync(Ingredient ingredient);
+    }
+}
