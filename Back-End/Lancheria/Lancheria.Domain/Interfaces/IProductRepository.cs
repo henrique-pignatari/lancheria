c4edcf4ff58c485a3858861e8ceb6fa0372db1f5
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-09-22 01:31:54 -0300

    feat: add product repository interface

---
+++ Back-End/Lancheria/Lancheria.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Lancheria.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lancheria.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetByIdAsync(int id);
+        Task<Product> CreateAsync(Product product);
+        Task<Product> UpdateAsync(Product product);
+        Task<Product> DeleteAsync(Product product);
+    }
+}
