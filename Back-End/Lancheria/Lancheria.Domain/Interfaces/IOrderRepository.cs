2f89355a99664b56b03d455464cb55bc0cbc7b47
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-09-22 01:32:39 -0300

    feat: add order repository interface

---
+++ Back-End/Lancheria/Lancheria.Domain/Interfaces/IOrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using Lancheria.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lancheria.Domain.Interfaces
+{
+    public interface IOrderRepository
+    {
+        Task<IEnumerable<Order>> GetOrdersAsync();
+        Task<Order> GetByIdAsync(int id);
+        Task<Order> CreateAsync(Order order);
+        Task<Order> UpdateAsync(Order order);
+        Task<Order> DeleteAsync(Order order);
+    }
+}
