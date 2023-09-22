6a3426922138dcb34ec2da5aaf088bd24f7460eb
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-09-22 01:32:14 -0300

    feat: add category repository interface

---
+++ Back-End/Lancheria/Lancheria.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using Lancheria.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lancheria.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategorysAsync();
+        Task<Category> GetByIdAsync(int id);
+        Task<Category> CreateAsync(Category category);
+        Task<Category> UpdateAsync(Category category);
+        Task<Category> DeleteAsync(Category category);
+    }
+}
